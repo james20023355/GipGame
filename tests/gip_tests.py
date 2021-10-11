4494e85b1ee7e6821279191337fc7e4ef2c317d7
JeiMZ <J.MEP@outlook.com>
2021-10-11 16:50:07 +0800

    Added a singular test on the dice roll

--- tests/gip_tests.py
+++ tests/gip_tests.py
@@ -1,1 +1,16 @@
 """ This File contains the unit tests for the gip game """
+import unittest
+from game import gip
+
+
+class GipGameTests(unittest.TestCase):
+
+    def test_diceRoll(self):
+
+        # Arrange
+        dice = gip.Dice()
+        # Act
+        result = gip.Dice.roll(dice)
+        # Assert
+        self.assertIsNotNone(result)
+
