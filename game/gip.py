3cbd6bbb274f176ff3009bf4d957f1ca07d62b21
JeiMZ <J.MEP@outlook.com>
2021-10-13 00:44:23 +0800

    Formatted output text, added player turn change and update score

--- game/gip.py
+++ game/gip.py
@@ -2,7 +2,7 @@
 This File Contains the code for the Git Game
 Author: James Penales
 """
-
+import time
 
 class Player:
     """ This class is to represent a real world player"""
@@ -12,8 +12,7 @@ class Player:
         self.score = 0
 
     def updateScore(self, score):
-
-        self.turn += score
+        self.score += score
 
 
 
@@ -24,27 +23,50 @@ class GipGame:
         self.turnScore = 0
 
 
-    def switchTurns(self, player, n):
+    def switchTurns(self, player, player2, n):
         """ This method is used to switch the players' turns """
         if not n:
-            rollAgain = input("Would you like to roll again?[Y/N]: ").upper()
+            rollAgain = input(f"> [GipGame]({self.turnScore}) Would you like to roll again?[Y/N]: ").upper()
             if rollAgain == 'Y':
-                result =  self.rollDice()
-                print(result)
+                result = self.rollDice()
+
                 if result != False:
-                    print("Your Rolled A: ", result)
+                    print("> [GipGame] You rolled a: ", result,"\n")
                 else:
-                    self.switchTurns(player, True)
-            elif rollAgain == 'N':
+                    self.switchTurns(player, player2,  True)
+
+            elif rollAgain == 'N' and player2.turn:
+                    player.turn = True
+                    player2.turn = False
+                    print(f"\n> [{player2.name}]  Congrats you earned: {self.turnScore} Points")
+                    player2.updateScore(self.turnScore)
+                    print(f"> [{player2.name}]  Your Current Score is: {player2.score}\n")
+                    self.turnScore = 0
+
+            elif rollAgain == 'N' and player.turn:
                 player.turn = False
+                player2.turn = True
+                print(f"\n> [{player.name}] Congrats you earned: {self.turnScore} Points")
+                player.updateScore(self.turnScore)
+                print(f"> [{player.name}] Your Current Score is: {player.score}\n")
+                self.turnScore = 0
             else:
                 print("Error: Wrong Input!")
-                self.switchTurns(player, False)
+                self.switchTurns(player, player2, False)
 
         else:
-            print(f"Sorry  Player: {player.name}, you rolled a 1. You lost all your points for this turn :(")
+
+            if player.turn:
+                print(f"Sorry {player.name}, you rolled a 1. You lost all your points for this turn :(\n")
+                player.turn = False
+                player2.turn = True
+
+            elif player2.turn:
+                print(f"Sorry {player2.name}, you rolled a 1. You lost all your points for this turn :(\n")
+                player.turn = True
+                player2.turn = False
             self.turnScore = 0
-            player.turn = False
+
 
 
 
@@ -61,26 +83,43 @@ class GipGame:
 
 
     def play(self):
-        print("> Hello Welcome to Git Game:")
-        p1Name = input("> Hello Player 1, Please Enter your Name: ")
-        p2Name = input("> Hello Player 2, Please Enter your Name: ")
+        self.currentlyPlaying = True;
+        print("> [GipGame] Hello and Welcome to Gip Game.")
+        time.sleep(2)
+
+        p1Name = input("> [GipGame] Player 1, Please Enter your Name: ")
+        time.sleep(0.5)
+        p2Name = input("> [GipGame] Player 2, Please Enter your Name: ")
+
+        time.sleep(2)
         player1 = Player(p1Name, True)
         player2 = Player(p2Name, False)
-        print(f"Welcome to the game {p1Name} and {p2Name}")
+        print(f"> [GipGame] Welcome to the game {p1Name} and {p2Name} \n")
+        time.sleep(2)
+        while self.currentlyPlaying:
+                if player1.turn:
+                    if self.turnScore == 0:
+                        firstRoll = self.rollDice()
 
-        while player1.turn:
-            if self.turnScore == 0:
-                firstRoll = self.rollDice()
+                        if firstRoll !=  False:
+                            print(f"> [{p1Name}]: Your first roll for this round is: ", firstRoll,"\n")
+                        else:
+                            self.switchTurns(player1, player2, True)
+                    else:
+                        self.switchTurns(player1, player2, False)
 
-                if firstRoll !=  False:
-                    print("Hello Player 1 your first roll is: ", )
-                    print(self.turnScore)
-                else:
-                    print(f"Sorry  Player: {p1Name}, you rolled a 1. Your turn ends")
-                    self.switchTurns(player1, True)
-            else:
-                self.switchTurns(player1, False)
-                print(self.turnScore)
+                time.sleep(1)
+                if player2.turn:
+                    if self.turnScore == 0:
+                        firstRoll = self.rollDice()
+
+                        if firstRoll != False:
+                            print(f"> [{p2Name}]: Your first roll for this round is: ", firstRoll,"\n")
+                        else:
+                            self.switchTurns(player1, player2, True)
+                    else:
+                        self.switchTurns(player1, player2, False)
+
 
 
 
@@ -101,7 +140,6 @@ class Dice:
 
 def main():
     game = GipGame()
-
     game.play()
 
 
